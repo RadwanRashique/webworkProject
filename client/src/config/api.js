a7135728eed671f67308f62168dced90653dd6cb
Radwan Rashique p <124045357+RadwanRashique@users.noreply.github.com>
2024-01-11 21:54:34 +0530

    Update api.js

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,5 +1,5 @@
 // src/config/api.js
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://webwork.website';
+const API_BASE_URL = 'https://webwork.website';
 
 export default {
   BASE_URL: API_BASE_URL,
